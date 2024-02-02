3803978b6744d76405edbcbac061f8b42ac4512e
Dawn Liu <dliu@lcs.on.ca>
2024-02-02 08:46:15 -0500

    Re-organized project and created a GameView file.

---
+++ GuessingGame/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  GuessingGame
+//
+//  Created by Chenxi Liu on 2024/2/2.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
